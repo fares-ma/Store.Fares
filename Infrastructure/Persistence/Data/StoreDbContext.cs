ed67f658be61c1c1934c0a23883e754d80c79a9b
fares-ma <168366189+fares-ma@users.noreply.github.com>
2025-04-13 03:59:15 +0200

    Done Doing Product Module - DbContext, Configurations,  Migrations

---
+++ Infrastructure/Persistence/Data/StoreDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Data
+{
+    // CLR
+    public class StoreDbContext : DbContext
+    {
+
+        public StoreDbContext(DbContextOptions<StoreDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductBrand> productBrands { get; set; }
+        public DbSet<ProductType> productTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AssemblyReference).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+
+    }
+    
+}
