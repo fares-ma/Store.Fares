62c3c8e69825d6492b08b709fcb1cd874e878941
fares-ma <168366189+fares-ma@users.noreply.github.com>
2025-04-13 17:07:05 +0200

    Create Product Service - Service Manager

---
+++ Core/Services.Abstractions/IServiceManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Abstractions
+{
+    public interface IServiceManager
+    {
+        IProductservice ProductService { get; }
+        //ITypeService TypeService { get; }
+        //IBrandService BrandService { get; }
+        //Task SaveAsync();
+    }
+}
---
+++ Shared/TypeResuItDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    public class TypeResuItDto
+    {
+        public int Id { get; set; } // PK
+        public string Name { get; set; }
+    }
+}
